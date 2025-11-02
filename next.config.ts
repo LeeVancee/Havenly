f4149b99ae3355095ca66c43709eb0916756110c
LEEVANCEE <1280403599@qq.com>
2025-11-02 08:25:43 +0800

    up

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
